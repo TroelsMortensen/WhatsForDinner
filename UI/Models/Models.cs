dcf48818661a0163a31dc2ae68c3cc8ee678e1fe
Troels Mortensen <tromortensen@hotmail.com>
2026-02-21 21:58:02 +0100

    updating models

--- UI/Models/Models.cs
+++ UI/Models/Models.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components;
 
 namespace UI.Models;
 
@@ -6,15 +6,14 @@ record Recipe(
     int Id,
     string Title,
     string? SubHeader,
-    int? TotalHours,
     int? TotalMinutes,
-    int? HoursOfPreparation,
     int? MinutesOfPreparation,
-    int? Persons,
+    int? NumberOfPersons,
     List<RecipeGroup> RecipeGroups,
     MarkupString Instructions,
     string Note,
-    string? ImageUrl
+    string? ImageUrl, 
+    List<Keyword> Keywords
 );
 
 record RecipeGroup(
@@ -29,3 +28,24 @@ record Ingredient(
     string Name,
     string? PreparationNote
 );
+
+class Keyword
+{
+    public string Value { get; private set; }
+    
+    private Keyword(string value) => Value = value;
+    
+    public static Keyword MOROCCAN { get; } = new ("Marokkansk");
+    public static Keyword SPANISH { get; } = new ("Spansk");
+    public static Keyword FRENCH { get; } = new ("Fransk");
+    public static Keyword ITALIAN { get; } = new ("Italiensk");
+    public static Keyword ONE_POT { get; } = new ("One Pot");
+    public static Keyword SLOW_COOKER { get; } = new ("Slow Cooker");
+    public static Keyword VEGETARIAN { get; } = new ("Vegetar");
+    public static Keyword FISH { get; } = new ("Fisk");
+    public static Keyword CHICKEN { get; } = new ("Kylling");
+    public static Keyword BEEF { get; } = new ("Oksekød");
+    public static Keyword SOUP { get; } = new ("Suppe");
+    public static Keyword PASTA { get; } = new ("Pasta");
+    public static Keyword ASIAN { get; } = new ("Asiatisk");
+}
