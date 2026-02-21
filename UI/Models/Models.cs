a9f922bae006dce01bce92a6e85c3eaa12ae0426
Troels Mortensen <tromortensen@hotmail.com>
2026-02-21 21:21:05 +0100

    initial commit

---
+++ UI/Models/Models.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace UI.Models;
+
+record Recipe(
+    int Id,
+    string Title,
+    string? SubHeader,
+    int? TotalHours,
+    int? TotalMinutes,
+    int? HoursOfPreparation,
+    int? MinutesOfPreparation,
+    int? Persons,
+    List<RecipeGroup> RecipeGroups,
+    MarkupString Instructions,
+    string Note,
+    string? ImageUrl
+);
+
+record RecipeGroup(
+    string Title,
+    List<Ingredient> Ingredients,
+    MarkupString Instructions
+);
+
+record Ingredient(
+    double? Quantity,
+    string? Unit,
+    string Name,
+    string? PreparationNote
+);
